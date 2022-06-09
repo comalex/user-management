6cd0506727a21c72eda2b936d455d2d85d884320
Alex Stupak <stupak.alexn@gmail.com>
2022-06-09 17:07:53 +0300

    refactor: added json parse for all methods

--- src/hooks/usePeople.ts
+++ src/hooks/usePeople.ts
@@ -1,40 +1,26 @@
 import { API } from 'aws-amplify';
-import { nanoid } from 'nanoid';
 
 import { showErrorNotification } from '../helpers/notification';
 
 export type TPerson = {
-  id: string;
   name: string;
   rank: number;
+};
+
+export type TPersonServer = TPerson & {
+  id: string;
 };
 
 const usePeople = () => {
   const API_NAME = 'peopleApi';
   const API_PATH = '/people';
-  const API_PARTIAL_KEY = 'id';
 
   const createPerson = (person: TPerson): Promise<any> => {
     return API.post(API_NAME, API_PATH, {
-      body: {
-        ...person,
-        id: nanoid(),
-      },
-    }).catch((err) => {
-      if (typeof err === 'string') {
-        showErrorNotification(err);
-      } else if (err.message && typeof err.message === 'string') {
-        showErrorNotification(err.message);
-      } else {
-        showErrorNotification('Unknown error');
-      }
-      return err;
-    });
-  };
-
-  const getPeople = (): Promise<any> => {
-    return API.get(API_NAME, `${API_PATH}/${API_PARTIAL_KEY}`, {}).catch(
-      (err) => {
+      body: person,
+    })
+      .then((res) => JSON.parse(res.body))
+      .catch((err) => {
         if (typeof err === 'string') {
           showErrorNotification(err);
         } else if (err.message && typeof err.message === 'string') {
@@ -43,51 +29,75 @@ const usePeople = () => {
           showErrorNotification('Unknown error');
         }
         return err;
-      }
-    );
+      });
+  };
+
+  const getPeople = (): Promise<any> => {
+    return API.get(API_NAME, API_PATH, {})
+      .then((res) => JSON.parse(res.body))
+      .catch((err) => {
+        if (typeof err === 'string') {
+          showErrorNotification(err);
+        } else if (err.message && typeof err.message === 'string') {
+          showErrorNotification(err.message);
+        } else {
+          showErrorNotification('Unknown error');
+        }
+        return err;
+      });
   };
 
   const getPerson = (id: string): Promise<any> => {
-    return API.get(API_NAME, `${API_PATH}/${id}`, {}).catch((err) => {
-      if (typeof err === 'string') {
-        showErrorNotification(err);
-      } else if (err.message && typeof err.message === 'string') {
-        showErrorNotification(err.message);
-      } else {
-        showErrorNotification('Unknown error');
-      }
-      return err;
-    });
+    return API.get(API_NAME, `${API_PATH}/${id}`, {})
+      .then((res) => JSON.parse(res.body))
+      .catch((err) => {
+        if (typeof err === 'string') {
+          showErrorNotification(err);
+        } else if (err.message && typeof err.message === 'string') {
+          showErrorNotification(err.message);
+        } else {
+          showErrorNotification('Unknown error');
+        }
+        return err;
+      });
   };
 
-  const updatePerson = (id: string, person: Partial<TPerson>): Promise<any> => {
-    return API.patch(API_NAME, `${API_PATH}/${id}`, {
+  const updatePerson = (
+    id: string,
+    person: Partial<TPersonServer>
+  ): Promise<any> => {
+    return API.put(API_NAME, API_PATH, {
       body: {
         ...person,
+        id,
       },
-    }).catch((err) => {
-      if (typeof err === 'string') {
-        showErrorNotification(err);
-      } else if (err.message && typeof err.message === 'string') {
-        showErrorNotification(err.message);
-      } else {
-        showErrorNotification('Unknown error');
-      }
-      return err;
-    });
+    })
+      .then((res) => JSON.parse(res.body))
+      .catch((err) => {
+        if (typeof err === 'string') {
+          showErrorNotification(err);
+        } else if (err.message && typeof err.message === 'string') {
+          showErrorNotification(err.message);
+        } else {
+          showErrorNotification('Unknown error');
+        }
+        return err;
+      });
   };
 
   const deletePerson = (id: string): Promise<any> => {
-    return API.del(API_NAME, `${API_PATH}/${id}`, {}).catch((err) => {
-      if (typeof err === 'string') {
-        showErrorNotification(err);
-      } else if (err.message && typeof err.message === 'string') {
-        showErrorNotification(err.message);
-      } else {
-        showErrorNotification('Unknown error');
-      }
-      return err;
-    });
+    return API.del(API_NAME, `${API_PATH}/${id}`, {})
+      .then((res) => JSON.parse(res.body))
+      .catch((err) => {
+        if (typeof err === 'string') {
+          showErrorNotification(err);
+        } else if (err.message && typeof err.message === 'string') {
+          showErrorNotification(err.message);
+        } else {
+          showErrorNotification('Unknown error');
+        }
+        return err;
+      });
   };
 
   return { createPerson, deletePerson, getPeople, getPerson, updatePerson };
