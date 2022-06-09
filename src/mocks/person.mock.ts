74dc04f7202990cd3e93596af88652d11c6584c6
Alex Stupak <stupak.alexn@gmail.com>
2022-06-09 22:42:09 +0300

    test: added test mocks

---
+++ src/mocks/person.mock.ts
@@ -0,0 +1,10 @@
+export const personMock = {
+  id: 'test-id',
+  name: 'John Doe',
+  rank: 1,
+};
+
+export const peopleMock = [
+  personMock,
+  { id: 'test-id-2', name: 'Jane Doe', rank: 2 },
+];
